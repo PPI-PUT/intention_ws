002d0eb00c85066c9a669082d1378316b18c53b0
Maciej Krupka <maciej.p.krupka@student.put.poznan.pl>
2023-12-18 17:38:48 +0100

    fix: update action
    Signed-off-by: Maciej Krupka <maciej.p.krupka@student.put.poznan.pl>

--- .github/scripts/setup.sh
+++ .github/scripts/setup.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ -f /opt/ros/humble/setup.bash ]; then source /opt/ros/humble/setup.bash; fi
-vcs import < default.repos
+vcs import < deps.repos
 sudo apt-get update
 rosdep update
 rosdep install --from-paths src --ignore-src -y
