bc7a8053448e35aa95e5b58eafa0c9832865aebd
Maciej Krupka <maciej.p.krupka@student.put.poznan.pl>
2023-12-22 11:14:26 +0100

    feat: add policy volume
    Signed-off-by: Maciej Krupka <maciej.p.krupka@student.put.poznan.pl>

--- run.sh
+++ run.sh
@@ -3,5 +3,6 @@ rocker --network host --privileged --nvidia --x11 --user --name unitree \
     --env "RMW_IMPLEMENTATION=rmw_fastrtps_cpp" \
     --env "ROS_DOMAIN_ID=0" \
     --volume "${PWD}:${HOME}/${PWD##*/}" \
+    --volume "${PWD}/../intention_policy:${HOME}/${PWD##*/}/../intention_policy" \
     --volume /dev/:/dev/ \
     -- macnack/unitree_ros:humble 
